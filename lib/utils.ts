7741403ce0dd088eadde471a08d4091fd02e0ba7
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-23 23:09:14 +0530

    feat(base64toBlob utility fn) 📍: Implement base64toBlob function to convert base64 encoded image data to Blob format, facilitating easy handling and manipulation of image data.

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,3 +18,15 @@ export function formatDate(d: Date) {
     ].join('-');
   return formattedDate;
 }
+
+export function base64toBlob(base64Data: any) {
+  const byteCharacters = atob(base64Data.split(',')[1]);
+  const arrayBuffer = new ArrayBuffer(byteCharacters.length);
+  const byteArray = new Uint8Array(arrayBuffer);
+
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteArray[i] = byteCharacters.charCodeAt(i);
+  }
+
+  return new Blob([arrayBuffer], { type: 'image/png' }); // Specify the image type here
+}
