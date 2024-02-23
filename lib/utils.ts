47412dcbd083f63dd23e0af392cdf98a9ba2f807
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-23 23:05:24 +0530

    feat(format Date utility fn) 🕰: Add formatDate function for formatting Date objects to YYYY-MM-DD HH:mm:ss format. This function ensures consistent and easily readable date formatting for various use cases.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(d: Date) {
+  const formattedDate =
+    [(d.getMonth() + 1).toString().padStart(2, '0'), d.getDate().toString().padStart(2, '0'), d.getFullYear()].join(
+      '-',
+    ) +
+    ' ' +
+    [
+      d.getHours().toString().padStart(2, '0'),
+      d.getMinutes().toString().padStart(2, '0'),
+      d.getSeconds().toString().padStart(2, '0'),
+    ].join('-');
+  return formattedDate;
 }
