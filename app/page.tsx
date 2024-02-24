630d49d1da8056e05dbae092ae58377ccff0ea56
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-24 14:38:03 +0530

    feat(RightContent): Implement feature highlights section with interactive buttons and descriptions in WatchTower interface.

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,23 @@
 import { ModeToggle } from '@/components/Theme-toggle';
 import { Button } from '@/components/ui/Button';
 import { Separator } from '@/components/ui/Separator';
-import { Camera, FlipHorizontal, PersonStanding, Video, Volume2 } from 'lucide-react';
-import React, { useRef, useState } from 'react'
+import { Camera, Divide, FlipHorizontal, MoonIcon, PersonStanding, SunIcon, Video, Volume2 } from 'lucide-react';
+
+import React, { useEffect, useRef, useState } from 'react'
 import Webcam from 'react-webcam';
-import { Rings } from 'react-loader-spinner';
+import { Hourglass, Rings } from 'react-loader-spinner';
 import { toast } from 'sonner';
 import { base64toBlob, formatDate } from '@/lib/utils';
 import { beep } from '@/lib/helpers/audio';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/Popover';
 import { Slider } from '@/components/ui/Slider';
+import SocialMediaLinks from '@/components/social-links';
+import { Meteors } from '@/components/ui/Meteors';
 
+import cocossd, { load } from '@tensorflow-models/coco-ssd'
+import "@tensorflow/tfjs-backend-cpu"
+import "@tensorflow/tfjs-backend-webgl"
+import { DetectedObject, ObjectDetection } from '@tensorflow-models/coco-ssd';
 type Props = {}
 
 let interval: any = null;
@@ -21,7 +28,7 @@ let stopTimeout: any = null;
 const HomePage = (props: Props) => {
     const webcamRef = useRef<Webcam>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    // state
+    // states
     const [mirrored, setMirrored] = useState<boolean>(true);
     const [isRecording, setIsRecording] = useState<boolean>(false);
     const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
@@ -30,7 +37,6 @@ const HomePage = (props: Props) => {
 
 
     return (
-
         <div className='flex h-screen'>
             {/* Left division - webcam and Canvas  */}
             <div className='relative'>
@@ -45,7 +51,7 @@ const HomePage = (props: Props) => {
                 </div>
             </div>
             {/* Righ division - container for button panel and wiki secion  */}
-            <div className='flex flex-row flex-1'>
+            <div className='flex flex-row flex-1  dark:bg-gray-900'>
                 {/* Top Part */}
                 <div className='border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4'>
                     {/* top secion  */}
@@ -109,13 +115,11 @@ const HomePage = (props: Props) => {
                             </PopoverContent>
                         </Popover>
                     </div>
-
                 </div>
-
-
-
+                <div className='h-full flex-1 py-4 px-4 overflow-y-scroll'>
+                    <RenderFeatureHighlightsSection />
+                </div>
             </div>
-
 
         </div>
 
@@ -176,6 +180,105 @@ const HomePage = (props: Props) => {
     }
 
     function toggleAutoRecord() { }
+
+    function RenderFeatureHighlightsSection() {
+        return <div className="text-xs text-muted-foreground relative items-center justify-center overflow-hidden rounded-md ">
+            <div className="w-full absolute inset-0 h-full ">
+                <Meteors number={20} />
+            </div>
+            <h1 className="md:text-2xl font-bold relative z-20 ">
+                WatchTower
+                {/* ai text in green */}
+                <span className="text-cyan-300"> AI</span>
+
+            </h1>
+
+            <Separator className='m-4' />
+
+            <ul className="space-y-4">
+                <li>
+                    <strong>Dark Mode/Sys Theme 🌗</strong>
+                    <p>Toggle between dark mode and system theme.</p>
+                    <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+                        <SunIcon size={14} />
+                    </Button>{" "}
+                    /{" "}
+                    <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+                        <MoonIcon size={14} />
+                    </Button>
+                </li>
+                <li>
+                    <strong>Horizontal Flip ↔️</strong>
+                    <p>Adjust horizontal orientation.</p>
+                    <Button className='h-6 w-6 my-2'
+                        variant={'outline'} size={'icon'}
+                        onClick={() => {
+                            setMirrored((prev) => !prev)
+                        }}
+                    ><FlipHorizontal size={14} /></Button>
+                </li>
+                <Separator />
+                <li>
+                    <strong>Take Pictures 📸</strong>
+                    <p>Capture snapshots at any moment from the video feed.</p>
+                    <Button
+                        className='h-6 w-6 my-2'
+                        variant={'outline'} size={'icon'}
+                        onClick={userPromptScreenshot}
+                    >
+                        <Camera size={14} />
+                    </Button>
+                </li>
+                <li>
+                    <strong>Manual Video Recording 📽️</strong>
+                    <p>Manually record video clips as needed.</p>
+                    <Button className='h-6 w-6 my-2'
+                        variant={isRecording ? 'destructive' : 'outline'} size={'icon'}
+                        onClick={userPromptRecord}
+                    >
+                        <Video size={14} />
+                    </Button>
+                </li>
+                <Separator />
+                <li>
+                    <strong>Enable/Disable Auto Record 🚫</strong>
+                    <p>
+                        Option to enable/disable automatic video recording whenever
+                        required.
+                    </p>
+                    <Button className='h-6 w-6 my-2'
+                        variant={autoRecordEnabled ? 'destructive' : 'outline'}
+                        size={'icon'}
+                        onClick={toggleAutoRecord}
+                    >
+                        {autoRecordEnabled ? <Rings color='white' height={30} /> : <PersonStanding size={14} />}
+
+                    </Button>
+                </li>
+
+                <li>
+                    <strong>Volume Slider 🔊</strong>
+                    <p>Adjust the volume level of the notifications.</p>
+                </li>
+                <li>
+                    <strong>Camera Feed Highlighting 🎨</strong>
+                    <p>
+                        Highlights persons in{" "}
+                        <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+                        <span style={{ color: "#00B612" }}>green</span>.
+                    </p>
+                </li>
+                <Separator />
+                <li className="space-y-4">
+                    <strong>Share your thoughts 💬 </strong>
+                    <SocialMediaLinks />
+                    <br />
+                    <br />
+                    <br />
+                </li>
+            </ul>
+        </div>
+    }
 
 }
 
