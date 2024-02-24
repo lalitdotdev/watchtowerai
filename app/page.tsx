767b458956d7bdcdacec9cd78c16f6d638132089
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-24 10:36:35 +0530

    feat(Control Panels 🎛 ): Implement right division containing button panel with ModeToggle and FlipHorizontal buttons.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 "use client"
 
 
+import { ModeToggle } from '@/components/Theme-toggle';
+import { Button } from '@/components/ui/Button';
+import { Separator } from '@/components/ui/Separator';
+import { Camera, FlipHorizontal, PersonStanding, Video } from 'lucide-react';
 import React, { useRef, useState } from 'react'
 import Webcam from 'react-webcam';
 
@@ -29,10 +33,25 @@ const HomePage = (props: Props) => {
                     ></canvas>
                 </div>
             </div>
+            {/* Righ division - container for button panel and wiki secion  */}
+            <div className='flex flex-row flex-1'>
+                <div className='border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4'>
+                    {/* top secion  */}
+                    <div className='flex flex-col gap-2'>
+                        <ModeToggle />
+                        <Button
+                            variant={'outline'} size={'icon'}
+                            onClick={() => {
+                                setMirrored((prev) => !prev)
+                            }}
+                        ><FlipHorizontal /></Button>
 
+                        <Separator className='my-2' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
-
 }
 
 export default HomePage
