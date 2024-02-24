fff2be2137918fc4077a65a7e87fbec556b8cacf
Lalit sharma <geekymonkcp742k@gmail.com>
2024-02-24 16:32:57 +0530

    final build trigger and dependencies and RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "aiwatchtower",
+    title: "aiwatchtower | ObjectDetection",
     description: "An AI-powered Next.js 14 web application with advanced people detection and recording capabilities. Experience real-time monitoring and seamless recording features for enhanced security and surveillance. Powered by cutting-edge technology, this app ensures efficient detection and tracking while providing a user-friendly interface for effortless management.",
 };
 
